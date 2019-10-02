068c1525f7ad4fea6b182e1f9bedd1884b6e4ae5
gatcuddy <emmettglaser@gmail.com>
2019-10-01 21:34:34 -0700

    actually did nothing

--- vao.h
+++ vao.h
@@ -2,15 +2,26 @@
 #include <GL/glew.h>
 #include <gl/GL.h>
 #include <gl/GLU.h>
+#include <memory>
+#include <initializer_list>
+#include <vector>
 
 namespace CNGE7
 {
 	class Attribute
 	{
 	public:
+		int per_vertex;
+		std::unique_ptr<float[]> data;
+		GLuint buffer;
 
-	private:
+		Attribute(int _per_vertex, float* _data)
+		{
+			per_vertex = _per_vertex;
+			data = std::unique_ptr<float[]>(_data);
 
+			glGenBuffers(1, &buffer);
+		}
 	};
 
 	class VAO
@@ -21,6 +32,7 @@ namespace CNGE7
 		{
 			glCreateVertexArrays(1, &vao);
 
+			auto at = Attribute(3, new float[2]{0.0f,1.0f});
 		}
 
 		void add_attribute(Attribute attribute)
@@ -29,5 +41,13 @@ namespace CNGE7
 		}
 	private:
 		GLuint vao;
+		GLuint ibo;
+
+		int vertex_count;
+		int attrib_count;
+
+		std::unique_ptr<Attribute> attribs;
+
+		int draw_mode;
 	};
 }
